1985eeaa5545f10d451a1161ded1ebe2fa4bb70d
alpha <alphamodeyt@gmail.com>
2025-11-10 11:29:17 -0600

    Fix TagDifferencePresentCondition not working on first load Fixes cobalt ore not being able to be melted when first loading into a world Fixes #130

--- src/main/java/slimeknights/tconstruct/library/json/TagDifferencePresentCondition.java
+++ src/main/java/slimeknights/tconstruct/library/json/TagDifferencePresentCondition.java
@@ -4,6 +4,7 @@ import com.google.common.collect.Iterables;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import net.fabricmc.fabric.api.resource.conditions.v1.ConditionJsonProvider;
+import net.fabricmc.fabric.impl.resource.conditions.ResourceConditionsImpl;
 import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
@@ -12,20 +13,24 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
+import org.jetbrains.annotations.Nullable;
 import slimeknights.mantle.util.JsonHelper;
 import slimeknights.tconstruct.TConstruct;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /** Condition requiring that items exist in the intersection of all required item tags */
 public class TagDifferencePresentCondition<T> implements ConditionJsonProvider {
   public static final ResourceLocation NAME = TConstruct.getResource("tag_difference_present");
 
-  private final TagKey<Item> base;
-  private final List<TagKey<Item>> subtracted;
+  private final TagKey<T> base;
+  private final List<TagKey<T>> subtracted;
 
-  public TagDifferencePresentCondition(TagKey<Item> base, List<TagKey<Item>> subtracted) {
+  public TagDifferencePresentCondition(TagKey<T> base, List<TagKey<T>> subtracted) {
     if (subtracted.isEmpty()) {
       throw new IllegalArgumentException("Cannot create a condition with no subtracted");
     }
@@ -35,13 +40,13 @@ public class TagDifferencePresentCondition<T> implements ConditionJsonProvider {
 
   /** Creates a condition from a set of keys */
   @SafeVarargs
-  public static TagDifferencePresentCondition<Item> ofKeys(TagKey<Item> base, TagKey<Item>... subtracted) {
+  public static <T> TagDifferencePresentCondition<T> ofKeys(TagKey<T> base, TagKey<T>... subtracted) {
     return new TagDifferencePresentCondition<>(base, Arrays.asList(subtracted));
   }
 
   /** Creates a condition from a registry and a set of names */
-  public static TagDifferencePresentCondition ofNames(ResourceKey<? extends Registry<Item>> registry, ResourceLocation base, ResourceLocation... subtracted) {
-    TagKey<Item> baseKey = TagKey.create(registry, base);
+  public static <T> TagDifferencePresentCondition<T> ofNames(ResourceKey<? extends Registry<T>> registry, ResourceLocation base, ResourceLocation... subtracted) {
+    TagKey<T> baseKey = TagKey.create(registry, base);
     return new TagDifferencePresentCondition<>(baseKey, Arrays.stream(subtracted).map(name -> TagKey.create(registry, name)).toList());
   }
 
@@ -52,8 +57,8 @@ public class TagDifferencePresentCondition<T> implements ConditionJsonProvider {
 
   public boolean test() {
     // get the base tag
-    Iterable<Holder<Item>> base = BuiltInRegistries.ITEM.getTagOrEmpty(this.base);
-    if (base == null || Iterables.isEmpty(base)) {
+    Collection<Holder<T>> base = getTag(this.base);
+    if (base == null || base.isEmpty()) {
       return false;
     }
 
@@ -65,11 +70,11 @@ public class TagDifferencePresentCondition<T> implements ConditionJsonProvider {
     }
     // all tags have something, so find the first item that is in all tags
     itemLoop:
-    for (Holder<Item> entry : base) {
+    for (Holder<T> entry : base) {
       // find the first item contained in no subtracted tags
-      for (TagKey<Item> tag : subtracted) {
+      for (TagKey<T> tag : subtracted) {
         // TODO: will this work?
-        if (Iterables.contains(BuiltInRegistries.ITEM.getTagOrEmpty(tag), entry)) {
+        if (getTag(tag).contains(entry)) {
           continue itemLoop;
         }
       }
@@ -80,9 +85,27 @@ public class TagDifferencePresentCondition<T> implements ConditionJsonProvider {
     return false;
   }
 
+  private Collection<Holder<T>> getTag(TagKey<T> tag) {
+    // cursed fabric internals
+    @Nullable
+    Map<ResourceKey<?>, Map<ResourceLocation, Collection<Holder<?>>>> allTags = ResourceConditionsImpl.LOADED_TAGS.get();
+
+    if (allTags == null) {
+      ResourceConditionsImpl.LOGGER.warn("Can't retrieve deserialized tags. Failing tconstruct:tag_difference_present resource condition check.");
+      return Set.of();
+    }
+    Map<ResourceLocation, Collection<Holder<T>>> tags = (Map) allTags.get(this.base.registry());
+
+    if (tags == null) {
+      return Set.of();
+    }
+
+    return tags.getOrDefault(tag.location(), Set.of());
+  }
+
   @Override
   public void writeParameters(JsonObject json) {
-    json.addProperty("registry", Registries.ITEM.location().toString());
+    json.addProperty("registry", this.base.registry().location().toString());
     json.addProperty("base", this.base.location().toString());
     JsonArray names = new JsonArray();
     for (TagKey<?> name : this.subtracted) {
@@ -91,8 +114,8 @@ public class TagDifferencePresentCondition<T> implements ConditionJsonProvider {
     json.add("subtracted", names);
   }
 
-  public static TagDifferencePresentCondition readGeneric(JsonObject json) {
-    ResourceKey<Registry<Item>> registry = ResourceKey.createRegistryKey(JsonHelper.getResourceLocation(json, "registry"));
+  public static <T> TagDifferencePresentCondition<T> readGeneric(JsonObject json) {
+    ResourceKey<Registry<T>> registry = ResourceKey.createRegistryKey(JsonHelper.getResourceLocation(json, "registry"));
     return new TagDifferencePresentCondition<>(
       TagKey.create(registry, JsonHelper.getResourceLocation(json, "base")),
       JsonHelper.parseList(json, "subtracted", (e, s) -> TagKey.create(registry, JsonHelper.convertToResourceLocation(e, s))));
